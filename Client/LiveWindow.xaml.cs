e9319d386ea755f54ec2c7451d183d33f9c41480
Yvanzhao <1359472155@qq.com>
2020-04-24 14:22:16 +0800

    打包发布

--- Client/LiveWindow.xaml.cs
+++ Client/LiveWindow.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using AForge.Video.DirectShow;
-using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using Unosquare.FFME.Common;
 using System.Threading;
